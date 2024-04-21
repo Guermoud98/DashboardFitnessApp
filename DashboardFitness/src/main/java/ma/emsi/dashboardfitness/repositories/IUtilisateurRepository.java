e4e090cca886b2fb879ac997b52fbda78d519252
@Neha-ETTALEBY <ettaleby.neha@gmail.com>
2024-04-21 21:19:30 +0100

    Added method in IUtilisateurRepository.java

--- DashboardFitness/src/main/java/ma/emsi/dashboardfitness/repositories/IUtilisateurRepository.java
+++ DashboardFitness/src/main/java/ma/emsi/dashboardfitness/repositories/IUtilisateurRepository.java
@@ -3,4 +3,6 @@ import ma.emsi.dashboardfitness.entities.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IUtilisateurRepository extends JpaRepository<Utilisateur, Long> {
+    Utilisateur findByEmail(String email);
+ //les autres methodes du CRUD existent par défaut  dans JpaRepository
 }
