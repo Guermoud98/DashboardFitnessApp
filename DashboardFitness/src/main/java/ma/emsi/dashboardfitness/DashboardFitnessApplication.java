6d9a3c4345ccad8535b8018067e4551d14b67e4d
Gur <guermoud.@gmail.com>
2024-04-21 11:06:07 +0000

    Create packages: entities, repositories and services. Configure database connection using Hibernate.

---
+++ DashboardFitness/src/main/java/ma/emsi/dashboardfitness/DashboardFitnessApplication.java
@@ -0,0 +1,14 @@
+package src.main.java.ma.emsi.dashboardfitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardFitnessApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DashboardFitnessApplication.class, args);
+    }
+
+}
