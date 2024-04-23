54727a86a1a22e5acd04233efd94ab76c6115cf1
Gur <guermoud.@gmail.com>
2024-04-23 22:33:08 +0000

    Added afficherExercices method to the ExerciceService class

--- DashboardFitness/src/main/java/ma/emsi/dashboardfitness/services/ExerciceService.java
+++ DashboardFitness/src/main/java/ma/emsi/dashboardfitness/services/ExerciceService.java
@@ -4,6 +4,8 @@ import ma.emsi.dashboardfitness.entities.Exercice;
 import ma.emsi.dashboardfitness.entities.Nutrition;
 import ma.emsi.dashboardfitness.repositories.IExerciceRepository;
 
+import java.util.List;
+
 public class ExerciceService {
     private IExerciceRepository exerciceRepository;
 
@@ -18,5 +20,30 @@ public class ExerciceService {
     //supprimer un exercice par l'admin de la base de donnee
     public void supprimerexercice(Exercice exercice) {
         exerciceRepository.delete(exercice);
+    }
+    //supprimer un exercice en se basant sur son ID
+    public void supprimerExerciceById(Long id) {
+        boolean existingExercice = exerciceRepository.findById(id).isPresent();
+        if(existingExercice) {
+            System.out.println("suppression de l'exercice qui a l'id: " + id);
+            exerciceRepository.deleteById(id);
+        }
+    }
+    // update d'un exercice
+    public void modifierExercice(Long id , Exercice updatedExercice) {
+        Exercice existingExercice = exerciceRepository.findById(id).orElse(null);
+        if(existingExercice != null) {
+            existingExercice.setDescription(updatedExercice.getDescription());
+            existingExercice.setDuree(updatedExercice.getDuree());
+            existingExercice.setEntrainement(updatedExercice.getEntrainement());
+            existingExercice.setImage(updatedExercice.getImage());
+            existingExercice.setNom(updatedExercice.getNom());
+            existingExercice.setNombreDeRep(updatedExercice.getNombreDeRep());
+            exerciceRepository.save(existingExercice);
+        }
+    }
+    //afficher tous les exercices
+    public List<Exercice > afficherExercices() {
+        return exerciceRepository.findAll();
     }
 }
