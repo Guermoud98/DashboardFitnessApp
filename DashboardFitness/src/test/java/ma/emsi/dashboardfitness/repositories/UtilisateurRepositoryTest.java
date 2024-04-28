7c0d6752d4d3f5ab992956f42897be271a74abc4
@Neha-ETTALEBY <ettaleby.neha@gmail.com>
2024-04-29 00:22:24 +0100

    Compeleted the test of method FindByEmail in UtilisateurRepositoryTest.java

--- DashboardFitness/src/test/java/ma/emsi/dashboardfitness/repositories/UtilisateurRepositoryTest.java
+++ DashboardFitness/src/test/java/ma/emsi/dashboardfitness/repositories/UtilisateurRepositoryTest.java
@@ -1,42 +1,44 @@
 package ma.emsi.dashboardfitness.repositories;
-
-import jakarta.persistence.Table;
 import ma.emsi.dashboardfitness.entities.Utilisateur;
-import org.assertj.core.api.Assertions;
-import org.junit.Test;
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+
 public class UtilisateurRepositoryTest {
-
 @Autowired
-    private  IUtilisateurRepository utilisateurRepository;
-@Test
-//
-    public void IUtilisateurRepository_SaveAll_ReturnSavedUtilisateur() {
-    //given
-    Utilisateur utilisateur =Utilisateur.builder()
-            .idUtilisateur(null)
-            .nom("ett")
-            .prenom("neha")
-            .poids(68)
-            .taille(161)
-            .email("utilisateur@example.com")
-            .password("Password123!")
-            .build();
-    //when
-    Utilisateur utilisateurSaved = utilisateurRepository.save(utilisateur);
-    //assert
-    Assertions.assertThat(utilisateurSaved).isNotNull();
-    Assertions.assertThat(utilisateurSaved.getIdUtilisateur()).isGreaterThan(0);
+    private IUtilisateurRepository utilisateurRepository;
+ @BeforeEach
+    public void setUp() {
+     utilisateurRepository.save(Utilisateur.builder().prenom("ettaleby").nom("neha").email("neha@example.com").password("Neha123!").poids(63).taille(162).build());
+     utilisateurRepository.save(Utilisateur.builder().prenom("ettaleby").nom("leila").email("leila@example.com").password("Leila123!").poids(63).taille(162).build());
+     utilisateurRepository.save(Utilisateur.builder().prenom("ettaleby").nom("houda").email("houda@example.com").password("Houda123!").poids(63).taille(162).build());
 
+ }
+ @Test
+ public void Should_Find_Utilisateur_ByEmail() {
 
-}
+     String givenEmail = "leila@example.com";
+     Utilisateur result = utilisateurRepository.findByEmail(givenEmail);
+     AssertionsForClassTypes.assertThat(result).isNotNull();
 
+ }
+    @Test
+    public void Should_Not_Find_Utilisateur_ByEmail() {
 
+        String givenEmail = "xxx@xxxxx.com";
+        Utilisateur result = utilisateurRepository.findByEmail(givenEmail);
+        AssertionsForClassTypes.assertThat(result).isNull();
+
+    }
 
 }
