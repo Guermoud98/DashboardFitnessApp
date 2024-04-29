fa5fb77e87d1c3fd3bba6a1d4a019af13724329e
@Neha-ETTALEBY <ettaleby.neha@gmail.com>
2024-04-29 14:22:44 +0100

    Deleted the overrided method equals

--- DashboardFitness/src/main/java/ma/emsi/dashboardfitness/entities/Entrainement.java
+++ DashboardFitness/src/main/java/ma/emsi/dashboardfitness/entities/Entrainement.java
@@ -22,17 +22,6 @@ public class Entrainement { //workout : si l'objectif d'utilisateur  est perte/g
     @JoinColumn(name="idNutrition")
     private Nutrition nutrition;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Entrainement that = (Entrainement) o;
-        return Objects.equals(nom, that.nom) &&
-                Objects.equals(duree, that.duree) &&
-                Objects.equals(description, that.description) &&
-                Objects.equals(imcMin, that.imcMin) &&
-                Objects.equals(imcMax, that.imcMax) &&
-                Objects.equals(nutrition, that.nutrition);
-    }
+
 
 }
