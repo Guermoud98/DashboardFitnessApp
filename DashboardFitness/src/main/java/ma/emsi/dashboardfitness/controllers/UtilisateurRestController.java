b2988612c1088cffc714cf59c18fe0e8913be5ce
@Neha-ETTALEBY <ettaleby.neha@gmail.com>
2024-04-30 22:58:41 +0100

    Added getAllUtilisateurs in UtilisateurRestController.java

--- DashboardFitness/src/main/java/ma/emsi/dashboardfitness/controllers/UtilisateurRestController.java
+++ DashboardFitness/src/main/java/ma/emsi/dashboardfitness/controllers/UtilisateurRestController.java
@@ -17,11 +17,18 @@ public class UtilisateurRestController {
 
     @Autowired
     private UtilisateurService utilisateurService;
+    @Autowired
+    private IUtilisateurRepository utilisateurRepository;
 
 
     @PostMapping(path = "/api/Utilisateurs/create/utilisateur")
     public ResponseEntity<Utilisateur> createUser(@RequestBody Utilisateur utilisateur) {
         Utilisateur createdUtilisateur = utilisateurService.Register(utilisateur);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdUtilisateur);
+    }
+    @GetMapping(path = "/api/Utilisateurs/getAll")
+    public ResponseEntity<List<Utilisateur>> getAllUtilisateurs() {
+        List<Utilisateur> utilisateurs = utilisateurRepository.findAll();
+        return ResponseEntity.ok(utilisateurs);
     }
 }
