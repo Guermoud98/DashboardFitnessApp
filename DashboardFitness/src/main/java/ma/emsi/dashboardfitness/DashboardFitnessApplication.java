d4a154cd10cda7e0ebeaa39149aa3ebcda6981a0
@Neha-ETTALEBY <ettaleby.neha@gmail.com>
2024-04-30 22:55:18 +0100

    nothing changed

--- DashboardFitness/src/main/java/ma/emsi/dashboardfitness/DashboardFitnessApplication.java
+++ DashboardFitness/src/main/java/ma/emsi/dashboardfitness/DashboardFitnessApplication.java
@@ -2,7 +2,10 @@
 package ma.emsi.dashboardfitness;
 
 import ma.emsi.dashboardfitness.entities.Nutrition;
+import ma.emsi.dashboardfitness.entities.Utilisateur;
 import ma.emsi.dashboardfitness.repositories.INutritionRepository;
+import ma.emsi.dashboardfitness.repositories.IUtilisateurRepository;
+import ma.emsi.dashboardfitness.services.UtilisateurService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,14 +13,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class DashboardFitnessApplication{
-
-
+public class DashboardFitnessApplication {
 
     public static void main(String[] args) {
 
         SpringApplication.run(DashboardFitnessApplication.class, args);
     }
+
+
 
 
 }
