f97f3831ffca926efc160a20b174ac05ec617472
Gur <guermoud.@gmail.com>
2024-05-01 17:03:36 +0000

    Added afficherNutritions method to the AdministrateurService of the service package

--- DashboardFitness/src/main/java/ma/emsi/dashboardfitness/services/AdministrateurService.java
+++ DashboardFitness/src/main/java/ma/emsi/dashboardfitness/services/AdministrateurService.java
@@ -2,16 +2,22 @@ package ma.emsi.dashboardfitness.services;
 
 
 import ma.emsi.dashboardfitness.entities.Administrateur;
+import ma.emsi.dashboardfitness.entities.Nutrition;
 import ma.emsi.dashboardfitness.repositories.IAdministrateurRepository;
+import ma.emsi.dashboardfitness.repositories.INutritionRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AdministrateurService {
     private IAdministrateurRepository administrateurRepository;
+    private INutritionRepository nutritionRepository;
     //couplage faible
     public AdministrateurService(IAdministrateurRepository administrateurRepository) {
         this.administrateurRepository = administrateurRepository;
     }
+    /******************************Admin login*************************************/
     public Administrateur AdminLogin(String username, String password) {
         Administrateur administrateur = administrateurRepository.findByLoginAndPassword(username,password);
         if(administrateur != null) {
@@ -19,6 +25,10 @@ public class AdministrateurService {
             return administrateur;
         }
         return null;
+    }
+    /******************************Nutrition*************************************/
+    public List<Nutrition> afficherNutritions() {
+        return nutritionRepository.findAll();
     }
 
 }
