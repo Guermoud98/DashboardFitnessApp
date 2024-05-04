2b452fe8b6e79ff6d0495ef6a66e1a8100e4b2df
@Neha-ETTALEBY <ettaleby.neha@gmail.com>
2024-05-04 21:19:08 +0100

    Added page login user

--- DashboardFitness/frontend/frontend-user/src/app/app.module.ts
+++ DashboardFitness/frontend/frontend-user/src/app/app.module.ts
@@ -1,21 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Importation du module d'animations de Angular
+import { MatFormFieldModule } from '@angular/material/form-field'; // Importation du module des champs de formulaire de Angular Material
+import { MatInputModule } from '@angular/material/input'; // Importation du module des champs d'entrée de Angular Material
+import { MatButtonModule } from '@angular/material/button'; // Importation du module des boutons de Angular Material
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { UserTemplateComponent } from './user-template/user-template.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import {MatIcon} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserTemplateComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule, // Ajout du module d'animations
+    AppRoutingModule,
+    MatFormFieldModule, // Ajout du module des champs de formulaire
+    MatInputModule, // Ajout du module des champs d'entrée
+    MatButtonModule,
+    MatIcon,
+    // Ajout du module des boutons
   ],
-  providers: [
-    provideAnimationsAsync()
-  ],
+  providers: [], // ici on declare les  services
   bootstrap: [AppComponent]
 })
 export class AppModule { }
